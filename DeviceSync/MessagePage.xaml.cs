f453bda1497f7483baf0c93240914afdeda73f6f
choosenname <vlad.luckashonok@ya.ru>
2023-03-29 13:08:36 +0300

    Небольшие изменения в структуре проекта

--- DeviceSync/MessagePage.xaml.cs
+++ DeviceSync/MessagePage.xaml.cs
@@ -1,3 +1,4 @@
+using DeviceSync.Messages;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -14,7 +15,7 @@ public partial class MessagePage : ContentPage
     {
         InitializeComponent();
 
-        ExceptionNotify += (sender, ex) => DisplayAlert("Exception", ex.Message + "\n" + ex.StackTrace, "OK");
+        ExceptionNotify += (sender, ex) => DisplayAlert("Exception", ex.ToString(), "OK");
 
         _udpSender = new UdpClient();
         _udpReceiver = new UdpClient(Singleton.Instance.LocalPort);
@@ -44,14 +45,34 @@ public partial class MessagePage : ContentPage
 
                     case PackageType.FileChunk:
                         {
-                            FileChunk fileMessage = receivedObject.ToObject<FileChunk>();
-
-
-
+                            BytesMessage fileMessage = receivedObject.ToObject<BytesMessage>();
+                            SaveFile(fileMessage);
                             break;
                         }
                 }
             }
+        }
+        catch (Exception ex)
+        {
+            ExceptionNotify?.Invoke(this, ex);
+        }
+    }
+    private async void SaveFile(BytesMessage fileMessage)
+    {
+        try
+        {
+            string fileName = Path.GetFileName(fileMessage.FileName);
+            string systemDownloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+
+            // Путь к файлу, который нужно сохранить
+            string filePathToSave = Path.Combine(systemDownloadsPath, fileName);
+
+            using (var fileStream = new FileStream(filePathToSave, FileMode.Create, FileAccess.Write))
+            {
+                await fileStream.WriteAsync(fileMessage.Content, 0, fileMessage.Content.Length);
+            }
+
+            AddMessageToChat($"Файл {fileName} сохранен!");
         }
         catch (Exception ex)
         {
@@ -63,7 +84,7 @@ public partial class MessagePage : ContentPage
     {
         try
         {
-            StringMessage stringMessage = new StringMessage(PackageType.Text, Message.Text);
+            StringMessage stringMessage = new(PackageType.Text, Message.Text);
 
             _udpSender.Connect(Singleton.Instance.IpAddress, Singleton.Instance.RemotePort);
 
@@ -92,7 +113,33 @@ public partial class MessagePage : ContentPage
         if (result != null)
         {
             string filePath = result.FullPath;
-            AddMessageToChat("Путь: " + filePath);
+
+            // Open the file and read its contents
+            using (FileStream fs = File.OpenRead(filePath))
+            {
+                byte[] buffer = new byte[1024]; // Buffer for the data to be sent
+                int bytesRead = 0; // Number of bytes read from the file
+
+                // Loop through the file and send its contents in chunks
+                while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    // Create a new BytesMessage to send the chunk of file
+                    BytesMessage fileMessage = new BytesMessage(PackageType.FileChunk, buffer.Take(bytesRead).ToArray(), Path.GetFileName(filePath));
+
+                    // Convert the message to a JSON string
+                    string jsonFragment = JsonConvert.SerializeObject(fileMessage);
+
+                    // Convert the JSON string to a byte array
+                    byte[] dataToSend = Encoding.UTF8.GetBytes(jsonFragment);
+
+                    // Display a message to the user to indicate that the packet was sent successfully
+                    AddMessageToChat("Packet sent successfully.");
+                }
+
+                // Display a message to the user to indicate that the file was sent successfully
+                AddMessageToChat("File sent successfully!");
+            }
         }
     }
+
 }
