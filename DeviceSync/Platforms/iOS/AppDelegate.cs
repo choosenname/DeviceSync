34cd2db69ff6142c28288fe7dc333eb33b5b806c
don't have any <vlad.luckashonok@ya.ru>
2023-06-21 23:04:50 +0300

    Delete project

--- DeviceSync/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace DeviceSync;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
